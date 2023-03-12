cfd277fff2f468a370e724c0f3c89c6c8f6c82d5
zohidbek-mengliboyev <uzrivojcoder@gmail.com>
2023-03-13 02:36:27 +0500

    Creating a Tailwind component

--- src/tailwind-container/index.ts
+++ src/tailwind-container/index.ts
@@ -1,10 +1,34 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
+import { normalize, strings } from "@angular-devkit/core";
+import {
+  apply,
+  applyTemplates,
+  chain,
+  mergeWith,
+  move,
+  Rule,
+  SchematicContext,
+  Tree,
+  url,
+} from "@angular-devkit/schematics";
+import { Schema } from "./schema";
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function tailwindContainer(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+export function tailwindContainer(_options: Schema): Rule {
+  return (_tree: Tree, _context: SchematicContext) => {
+    _options.path =
+      _options.path ?? normalize(("src/app/" + _options.name) as string);
+    const templateSource = apply(url("./files"), [
+      applyTemplates({
+        classify: strings.classify,
+        dasherize: strings.dasherize,
+        name: _options.name,
+      }),
+      move(normalize(_options.path as string)),
+    ]);
+
+    return chain([
+      mergeWith(templateSource)
+    ]);
   };
 }
